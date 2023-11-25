b5b79a98956dcfa8b9f892232b8eefe0c1ef46e2
Anchorquery <anchorquery@gmail.com>
2023-11-25 18:10:34 -0400

    Update conexion_common.dart

--- app/lib/common/conexion_common.dart
+++ app/lib/common/conexion_common.dart
@@ -1,6 +1,6 @@
 class ConexionCommon {
-  static const String hostG = 'http://192.168.0.109:1337/graphql';
-  static const String hostApi = 'http://192.168.0.112:1337/api';
-  static const String hostBase = 'http://192.168.0.112:1337';
+  static const String hostG = 'https://server2.testia.ddveloper.net/graphql';
+  static const String hostApi = 'https://server2.testia.ddveloper.net/api';
+  static const String hostBase = 'https://server2.testia.ddveloper.net';
   static const String token = 'http://';
 }
