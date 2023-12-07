b0989d0040fd209b29490edb668815f3d35f9c05
Anchorquery <anchorquery@gmail.com>
2023-12-07 07:24:24 -0400

    UPADTE IOS

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,12 +1,26 @@
 import UIKit
 import Flutter
-
+import flutter_local_notifications
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // This is required to make any communication available in the action isolate.
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+
+    GMSServices.provideAPIKey("AIzaSyBbluXRjZ7O3n3A6n5Wi01ePbDm9pXMCH4")
+    GeneratedPluginRegistrant.register(with: self)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
