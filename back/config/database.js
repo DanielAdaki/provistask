e121b24872e5f87b6d27aacd7ff3e903cb6ca54f
Anchorquery <anchorquery@gmail.com>
2023-12-07 13:56:52 -0400

    cxbfxdg

--- back/config/database.js
+++ back/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '193.203.167.168'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'server_provitask'),
       user: env('DATABASE_USERNAME', 'root'),
